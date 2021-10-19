cb7423a4204b7857fb69e2adeb35a13a660dd266
Jami Kousa <jami.kousa@helsinki.fi>
2021-10-19 14:35:05 +0300

    test post

--- tdd-mooc-webapp/backend/test/todo_api.test.js
+++ tdd-mooc-webapp/backend/test/todo_api.test.js
@@ -1,28 +1,38 @@
-const app = require('../app');
-const { expect } = require('chai');
-const request = require('supertest');
+const app = require("../app");
+const { expect } = require("chai");
+const request = require("supertest");
 
-const todopath = `/api/todos`
+const todopath = `/api/todos`;
 
-describe('Todos', function() {
-  it('should say hello', function(done) {
+describe("Todos", function () {
+  it("should say hello", function (done) {
     request(app)
-      .get('/api')
-      .end(function(err, res) {
-        expect(res.text).to.equal('Hello from backend');
+      .get("/api")
+      .end(function (_, res) {
+        expect(res.text).to.equal("Hello from backend");
         expect(res.statusCode).to.equal(200);
         done();
       });
   });
 
-  it('should respond with an empty array', function(done) {
+  it("should respond with an empty array", function (done) {
     request(app)
       .get(todopath)
-      .end(function(err, res) {
-        expect(res.body).to.be.an('array');
+      .end(function (_, res) {
+        expect(res.body).to.be.an("array");
         expect(res.statusCode).to.equal(200);
         done();
       });
   });
 
+  it("should accept a new todo", function (done) {
+    request(app)
+      .post(todopath)
+      .send({ text: "Say Hello" })
+      .end(function (_, res) {
+        expect(res.body.text).to.equal("Say Hello");
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+  });
 });
