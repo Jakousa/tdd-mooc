fcb052f238aa393eaf9a89d3915c1a7d673e59b3
Jami Kousa <jami.kousa@helsinki.fi>
2021-10-25 17:46:52 +0300

    todolist test

---
+++ tdd-mooc-webapp/frontend/src/TodoList.test.js
@@ -0,0 +1,10 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import TodoList from './TodoList';
+
+test('renders a list of todos', () => {
+  const todos = ['Hello 1', 'Hello 2'].map(text => ({ text }))
+  render(<TodoList todos={todos} />);
+  const linkElement = screen.getByText(/Hello 1/i);
+  expect(linkElement).toBeInTheDocument();
+});
