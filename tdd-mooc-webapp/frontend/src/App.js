d2f581bc742847b2f76f2066f399579fd29ee14c
Jami Kousa <jami.kousa@helsinki.fi>
2021-10-26 15:44:38 +0300

    App implement

--- tdd-mooc-webapp/frontend/src/App.js
+++ tdd-mooc-webapp/frontend/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import TodoList from './TodoList'
+import useGetTodos from './useGetTodos'
 
 const App = () => {
+  const todos = useGetTodos()
   return (
     <>
+      <TodoList todos={todos} />
       Hello from frontend
     </>
   )
