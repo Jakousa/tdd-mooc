6083bdf3eabbd701bf5de37d4846950794616a70
Jami Kousa <jami.kousa@helsinki.fi>
2021-11-02 13:51:30 +0200

    todolist test betterments

--- tdd-mooc-webapp/frontend/src/TodoList.test.js
+++ tdd-mooc-webapp/frontend/src/TodoList.test.js
@@ -5,6 +5,6 @@ import TodoList from './TodoList';
 test('renders a list of todos', () => {
   const todos = ['Hello 1', 'Hello 2'].map((text, idx) => ({ id: idx, text }))
   render(<TodoList todos={todos} />);
-  const linkElement = screen.getByText(/Hello 1/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/Hello 1/i)).toBeInTheDocument();
+  expect(screen.getByText(/Hello 2/i)).toBeInTheDocument();
 });
