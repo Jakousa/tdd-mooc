e3e6382c7c78a24ac267d3ea23c818890034abb9
Jami Kousa <jami.kousa@helsinki.fi>
2021-11-02 13:41:26 +0200

    advanced todo tests

--- tdd-mooc-webapp/frontend/src/Todo.test.js
+++ tdd-mooc-webapp/frontend/src/Todo.test.js
@@ -1,10 +1,45 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, within } from '@testing-library/react';
 import Todo from './Todo';
+import sinon from 'sinon'
+import * as useUpdateTodo from './useUpdateTodo'
+
+const todos = ['Hello 1'].map((text, idx) => ({ id: idx, text, completed: false }))
+
+const setup = () => {
+  const utils = render(<Todo todo={todos[0]} />);
+  const button = utils.getByLabelText('todo-completed-button')
+  return { button }
+}
 
 test('renders todo text', () => {
-  const todos = ['Hello 1', 'Hello 2'].map((text, idx) => ({ id: idx, text }))
-  render(<Todo todo={todos[0]} />);
+  setup()
   const linkElement = screen.getByText(/Hello 1/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('shows button if todo is not completed', () => {
+  const { button } = setup()
+  expect(within(button).getByText('Mark as completed')).toBeInTheDocument()
+})
+
+test('shows completed text if todo is completed', () => {
+  render(<Todo todo={{ ...todos[0], completed: true }} />);
+  const linkElement = screen.getByText(/Completed/i);
+  expect(linkElement).toBeInTheDocument();  
+})
+
+test('todo is updated by update button', () => {
+  const updateTodoStub = sinon.stub()
+  sinon.stub(useUpdateTodo, 'default').returns([todos[0], updateTodoStub])
+
+  const { button } = setup()
+
+  button.click()
+
+  expect(updateTodoStub.callCount).toEqual(1)
+});
+
+afterEach(() => {
+  sinon.restore()
+})
