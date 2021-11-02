06ed253f57d286312abc261fd07790695e76d692
Jami Kousa <jami.kousa@helsinki.fi>
2021-11-02 13:41:33 +0200

    implementation

--- tdd-mooc-webapp/frontend/src/Todo.js
+++ tdd-mooc-webapp/frontend/src/Todo.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import useUpdateTodo from './useUpdateTodo'
 
-const Todo = ({ todo }) => {
+const Todo = ({ todo: propTodo }) => {
+  const [todo, updateTodo] = useUpdateTodo(propTodo)
+
+  const markTodoAsCompleted = () => {
+    updateTodo(todo.id, { completed: true })
+  }
+
   return (
     <>
       {todo.text}
+      {todo.completed ?
+      "Completed"
+      : <button onClick={markTodoAsCompleted} aria-label="todo-completed-button">Mark as completed</button>
+      }
     </>
   )
 }
